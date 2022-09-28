18406e8228a6d149c1430f100a48d4231c6a2a1c
Treasure Ajefu <treasureajefu@gmail.com>
2022-09-28 12:53:40 +0100

    thedom update now works, i'll try to validate the input with html now

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 var nameInput = document.getElementById('nameInput');
-// var numberInput = document.getElementById('numberInput');
-// var monthInput = document.getElementById('monthInput');
-// var yearInput = document.getElementById('yearInput');
-// var cvcInput = document.getElementById('cvcInput');
+var numberInput = document.getElementById('numberInput');
+var monthInput = document.getElementById('monthInput');
+var yearInput = document.getElementById('yearInput');
+var cvcInput = document.getElementById('cvcInput');
 
-// var form = document.getElementById('form');
+var form = document.getElementById('form');
 
-// var cardName = document.getElementById('cardName');
-// var cardNumber = document.getElementById('cardNumber');
-// var cardExpiryMonth = document.getElementById('card-expiry-month');
-// var cardExpiryYear = document.getElementById('card-expiry-year');
-// var cvc = document.getElementById('cvc');
+var cardName = document.getElementById('card-name');
+var cardNumber = document.getElementById('card-number');
+var cardExpiryMonth = document.getElementById('card-expiry-month');
+var cardExpiryYear = document.getElementById('card-expiry-year');
+var cvc = document.getElementById('cvc');
 
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
@@ -18,5 +18,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
+    cardName.innerText = nameInput.value
+    cardNumber.innerText = numberInput.value
+    cardExpiryMonth.innerText = monthInput.value
+    cardExpiryYear.innerText = yearInput.value
+    cvc.innerText = cvcInput.value
     console.log(nameInput.value);
 });
